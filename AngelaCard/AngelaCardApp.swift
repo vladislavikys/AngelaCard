7e213cebc2d6c5ad44c49712f65ae4eb2d20646d
vlad <>
2022-09-01 12:26:43 +0300

    init

---
+++ AngelaCard/AngelaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngelaCardApp.swift
+//  AngelaCard
+//
+//  Created by vlad zarya on 1.09.22.
+//
+
+import SwiftUI
+
+@main
+struct AngelaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
